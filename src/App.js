7ae843199e6822912973b604f0c8378606bb729e
Pradeep Kundar <pradeepkundar04@gmail.com>
2022-11-15 13:33:18 +0530

    commits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./components/store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const ctx = useContext(AuthContext);
+  /*  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     const storedUserLoggedInfo = localStorage.getItem("isLoggedIn");
@@ -25,14 +27,14 @@ function App() {
   const logoutHandler = () => {
     setIsLoggedIn(false);
     localStorage.removeItem("isLoggedIn");
-  };
+  }; */
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
     </React.Fragment>
   );
